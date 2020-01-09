be0b22cf21b81a0d2e31be821bbc33eaac5600c9
Cristi Mitrana <mitu+retropie@fastmail.fm>
2020-01-09 14:35:17 +0200

    remove debug output

--- mkdocs_altlink_plugin/plugin.py
+++ mkdocs_altlink_plugin/plugin.py
@@ -3,22 +3,20 @@ from mkdocs.plugins import BasePlugin
 
 class AlternateLinkPlugin(BasePlugin):
 
-    MD_LINK = re.compile(r''']\(([^)]*)\)''', re.UNICODE)
+    MD_LINK = re.compile(r']\(([^)]*)\)', re.UNICODE)
 
     def _should_ignore_link(self, link):
-        return link.startswith("http") or link.startswith("#") or link.startswith("ftp") or link.startswith("www") or link.endswith(".md")
+        return link.startswith(u"http") or link.startswith(u"#") or link.startswith(u"ftp") or link.startswith(u"www") or link.endswith(u".md")
 
     def on_page_markdown(self, markdown, page, config, **kwargs):
 
-        print "Page: " + page.title
-
         links = re.findall(self.MD_LINK, markdown)
         for link in links:
-            if self._should_ignore_link(link.strip()):
+            if self._should_ignore_link(link.lstrip()):
                 continue
 
             # Get any anchors in the URL
-            vals = link.split("#")
+            vals = link.lstrip().split("#")
 
             # Check if it's a valid internal .md page URL
             if vals[0].endswith(".md"):
