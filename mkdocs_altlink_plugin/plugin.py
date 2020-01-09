f657db1e75a41cd007deadcb88340d2b2d20fc1a
cmitu <31816814+cmitu@users.noreply.github.com>
2020-01-09 19:55:37 +0200

    add an extra test to decide if the link is a page resource

--- mkdocs_altlink_plugin/plugin.py
+++ mkdocs_altlink_plugin/plugin.py
@@ -1,4 +1,7 @@
 import re
+import os
+
+from mkdocs.config import Config
 from mkdocs.plugins import BasePlugin
 
 class AlternateLinkPlugin(BasePlugin):
@@ -8,9 +11,14 @@ class AlternateLinkPlugin(BasePlugin):
     def _should_ignore_link(self, link):
         return link.startswith(u"http") or link.startswith(u"#") or link.startswith(u"ftp") or link.startswith(u"www") or link.endswith(u".md")
 
-    def on_page_markdown(self, markdown, page, config, **kwargs):
+    def _is_valid_file(self, path, root):
+        return os.path.exists(path) or os.path.exists(root + "/" + path)
+
+    def on_page_markdown(self, markdown, page = None, config = None, **kwargs):
 
         links = re.findall(self.MD_LINK, markdown)
+        doc_dir = config.data.get("docs_dir")
+
         for link in links:
             if self._should_ignore_link(link.lstrip()):
                 continue
@@ -22,7 +30,11 @@ class AlternateLinkPlugin(BasePlugin):
             if vals[0].endswith(".md"):
                 continue
 
-            # Append .md to the page name if not found
+            # Check if the link is not a resource linked (img/pdf/etc)
+            if self._is_valid_file(vals[0], doc_dir):
+                continue
+
+            # Append .md to the page name,this is an internal link candidate
             vals[0] += ".md"
 
             # Re-construct the link
