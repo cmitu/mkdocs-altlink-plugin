90a168cbd5b86a35514731ff735e907433692ed8
cmitu <31816814+cmitu@users.noreply.github.com>
2021-02-04 14:59:30 +0000

    Ignore links found in fenced code blocks

--- mkdocs_altlink_plugin/plugin.py
+++ mkdocs_altlink_plugin/plugin.py
@@ -1,12 +1,27 @@
 import re
 import os
 
+from textwrap import dedent
+
 from mkdocs.config import Config
 from mkdocs.plugins import BasePlugin
 
 class AlternateLinkPlugin(BasePlugin):
 
-    MD_LINK = re.compile(r']\(([^)]*)\)', re.UNICODE)
+    MD_LINK_RE = re.compile(r']\(([^)]*)\)', re.UNICODE)
+    FENCED_BLOCK_RE = re.compile(
+        dedent(r'''
+            (?P<fence>^(?:~{3,}|`{3,}))[ ]*                      # opening fence
+            ((\{(?P<attrs>[^\}\n]*)\})?|                         # (optional {attrs} or
+            (\.?(?P<lang>[\w#.+-]*))?[ ]*                        # optional (.)lang
+            (hl_lines=(?P<quot>"|')(?P<hl_lines>.*?)(?P=quot))?) # optional hl_lines)
+            [ ]*\n                                               # newline (end of opening fence)
+            (?P<code>.*?)(?<=\n)                                 # the code block
+            (?P=fence)[ ]*$                                      # closing fence
+        '''),
+        re.MULTILINE | re.DOTALL | re.VERBOSE
+    )
+
 
     def _should_ignore_link(self, link):
         return link.startswith(u"http") or link.startswith(u"#") or link.startswith(u"ftp") or link.startswith(u"www") or link.startswith(u"mailto") or link.endswith(u".md")
@@ -16,10 +31,17 @@ class AlternateLinkPlugin(BasePlugin):
 
     def on_page_markdown(self, markdown, page = None, config = None, **kwargs):
 
-        links = re.findall(self.MD_LINK, markdown)
         doc_dir = config.data.get("docs_dir")
 
-        for link in links:
+        # Get the list of code blocks present in the document
+        code_blocks = re.finditer(self.FENCED_BLOCK_RE, markdown)
+
+        offset = 0
+        out_markdown = markdown
+        for m in re.finditer(self.MD_LINK_RE, markdown):
+            is_fenced = False
+            link = m.string[m.start(1):m.end(1)]
+
             if self._should_ignore_link(link.lstrip()):
                 continue
 
@@ -30,17 +52,28 @@ class AlternateLinkPlugin(BasePlugin):
             if vals[0].endswith(".md"):
                 continue
 
+            # Check if the link is not part of a code block
+            for block in code_blocks:
+                if block.start() < m.start(1) and block.end() >= m.start(1):
+                    is_fenced = True
+                    break
+            if is_fenced:
+                continue
+
             # Check if the link is not a resource linked (img/pdf/etc)
             if self._is_valid_file(vals[0], doc_dir):
                 continue
 
-            # Append .md to the page name,this is an internal link candidate
+            # Append .md to the page name, this is an internal link candidate
             vals[0] += ".md"
 
             # Re-construct the link
             href_new = "#".join(vals)
 
             # Replace the values in the markdown
-            markdown = markdown.replace("({})".format(link), "({})".format(href_new))
+            out_markdown = out_markdown[:m.start(1) + offset] + href_new + out_markdown[m.end(1) + offset:]
 
-        return markdown
+            # Offset the matches with the replaced value's length
+            offset += len(href_new) - (m.end(1) - m.start(1))
+
+        return out_markdown
